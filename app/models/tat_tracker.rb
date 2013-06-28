b820d124e52b8b0f43cdd461681c34baf765887f
Tim Preston <134735@gmail.com>
2013-06-28 12:02:20 -0500

    Added carrierwave, able to upload pictures now, still need to add styling

--- app/models/tat_tracker.rb
+++ app/models/tat_tracker.rb
@@ -1,3 +1,4 @@
 class TatTracker < ActiveRecord::Base
   attr_accessible :artist_email, :artist_location, :artist_name, :artist_phone, :artist_twitter, :date_inked, :inspiration, :location_on_body, :picture, :username
+  mount_uploader :picture, PictureUploader
 end
