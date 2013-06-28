98e88edcce6efedf43dce3114492673bf4f9fdd0
Tim Preston <134735@gmail.com>
2013-06-28 10:05:37 -0500

    Initial COmmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TatTrack::Application.config.secret_token = '63aa665d5cfda9b996ccd6b6c543c7cfadbd53a45cc3aa45bfe0d3bbfe5c78fc4e2bc1f326497d9c4658cf60d35a64a132776ebdb33105eed826882e47f95dd9'
